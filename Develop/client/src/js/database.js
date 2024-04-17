bcba98ec479e1a422252516b95cb4b25a7522b6a
David Truong <Davidtruong134@gmail.com>
2024-04-16 20:15:42 -0500

    added todos

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
 
 // Add the content to the database
 const jateDb = openDB('jate', 1);
@@ -29,11 +29,31 @@ const request = store.put({id: 1, value: content});
 
 // Wait for the request to complete
 const result = await request;
-
+};
 
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
 
+// Get all the content from the database
+const jateDb = openDB('jate', 1);
+
+// Open a transaction
+const tx = jateDb.transaction('jate', 'readonly');
+
+// Get the store
+const store = tx.objectStore('jate');
+
+// Get all the content
+const request = store.getAll();
+
+// Wait for the request to complete
+const result = await request;
+console.log(result);
+
+// Return the content
+return result.value;
+
+};
 initdb();
